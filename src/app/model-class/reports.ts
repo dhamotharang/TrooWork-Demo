f4942c6eca68835c99d3b1dda7c89956a4136077
anjuanilkumar91 <anjuanilkumar.troodon@outlook.com>
2018-09-27 17:44:16 +0530

    barcode report generation

--- src/app/model-class/reports.ts
+++ src/app/model-class/reports.ts
@@ -11,20 +11,36 @@ export interface Reports {
     InspectionDate: Date;
     InspectionTime: Time;
     FacilityName: string;
+
     RoomId: string;
     RoomType: string;
+
     LastName: string;
     FirstName: string;
     EmployeeName: string;
     InspectionCompletedBy: number;
     id: number;
     totalItems: 10;
+
     EquipmentKey: number;
     EquipmentName: string;
     EquipmentTypeKey: number;
+    EquipmentType:string;
     EquipmentTypeText: string;
     EquipmentTypeDescription: string;
+    
     FacilityKey: number;
     FacilityText: string;
+
+    FloorKey:number;
+    FloorName:string;
+
+    ZoneKey:number;
+    ZoneName:string;
+
+    RoomTypeKey:number;
+    RoomTypeName:string;
+
+    Barcode1:number;
 
 }
