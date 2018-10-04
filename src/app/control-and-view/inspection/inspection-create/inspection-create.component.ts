dd3dbb53c67873415c1ccbc359f7d912c1a29982
poojah-89 <pooja.troodon@outlook.com>
2018-10-04 17:32:03 +0530

    commit includes works on Create Inspection

--- src/app/control-and-view/inspection/inspection-create/inspection-create.component.ts
+++ src/app/control-and-view/inspection/inspection-create/inspection-create.component.ts
@@ -8,6 +8,7 @@ import { Inspection } from '../../../model-class/Inspection';
   styleUrls: ['./inspection-create.component.scss']
 })
 export class InspectionCreateComponent implements OnInit {
+  marked = false;
   templateName: Inspection[];
   auditor: Inspection[];
   employee: Inspection[];
@@ -17,6 +18,13 @@ export class InspectionCreateComponent implements OnInit {
   room:Inspection[];
   roomtype:Inspection[];
   facikey: Number;
+  TemplateID: Number;
+  SupervisorKey: Number;
+  fromdate:Date;
+  todate:Date;
+  theCheckbox:any;
+  time:any;
+  RoomKey:Number;
 // adding properties and methods that will be used by the igxDatePicker
 public date: Date = new Date(Date.now());
 
@@ -26,6 +34,12 @@ private monthFormatter = new Intl.DateTimeFormat('en', { month: 'long'});
 public formatter = (_: Date) => {
     return `You selected ${this.dayFormatter.format(_)}, ${_.getDate()} ${this.monthFormatter.format(_)}, ${_.getFullYear()}`;
 }
+ convert_DT(str) {
+  var date = new Date(str),
+          mnth = ("0" + (date.getMonth() + 1)).slice( - 2),
+          day = ("0" + date.getDate()).slice( - 2);
+          return [date.getFullYear(), mnth, day].join("-");
+  };
 
   constructor(private inspectionService: InspectionService) { }
   selectFloorfromBuildings(facKey){
@@ -57,6 +71,29 @@ public formatter = (_: Date) => {
       this.roomtype = data;
     });
   }
+  
+  createInspection(TemplateID,SupervisorKey,fromdate,todate,theCheckbox,time,RoomKey) {
+    debugger;
+    var t=new Date();
+    var t=new Date();
+    var y=t.getFullYear();
+    var m=t.getMonth();
+    var d=t.getDate();
+    var h=t.getHours();
+    var mi=t.getMinutes();
+    var s=t.getSeconds();
+    
+         var today_DT = this.convert_DT(new Date());
+                    
+    //var x=new Date(t.getFullYear(),t.getMonth(),t.getDate()).join(-);
+    //console.log(x);
+    //console.log(y+"-"+m+"-"+d+" "+h+":"+mi+":"+s);
+    var p="";
+    p=today_DT+" "+h+":"+mi+":"+s;
+ 
+    this.inspectionService.createInspections(this.TemplateID,this.SupervisorKey,this.fromdate,this.todate,this.theCheckbox,this.time,this.RoomKey);
+    
+}
   ngOnInit() {
 
     this.inspectionService
@@ -84,5 +121,11 @@ public formatter = (_: Date) => {
       this.building = data;
     });
   }
-
+  toggleVisibility(e) {
+    if (e.target.checked) {
+      this.marked= true;
+    } else {
+      this.marked = false;
+    }
+  }
 }
