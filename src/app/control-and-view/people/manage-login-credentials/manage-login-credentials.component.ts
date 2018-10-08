832bc44b1bc86d472c6f5f319f609b71d1ab9253
Previn Kumar <previnkk@gmail.com>
2018-10-08 00:32:38 -0400

    updated People module import statments to match the directory name

--- src/app/control-and-view/people/manage-login-credentials/manage-login-credentials.component.ts
+++ src/app/control-and-view/people/manage-login-credentials/manage-login-credentials.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { People } from '../../../Model-Class/People';
+import { People } from '../../../model-class/People';
 import { PeopleServiceService } from '../../../service/people-service.service';
 
 @Component({
