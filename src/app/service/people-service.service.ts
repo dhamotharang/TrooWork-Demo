e32e953e80eb3c308b522dacefab855d183f900f
poojah-89 <pooja.troodon@outlook.com>
2018-11-26 17:34:54 +0530

    Issues fixed

--- src/app/service/people-service.service.ts
+++ src/app/service/people-service.service.ts
@@ -537,6 +537,12 @@ export class PeopleServiceService {
     return this
       .http.post(url, obj);
   }
+  getMeetingTrainingViewforemployees(curr_date,toServeremployeekey,OrganizationID)
+  {
+    return this
+    .http
+    .get('http://localhost:3000/api/viewEmployeeMeetingTraining?meetingDate='+curr_date+'&employeekey='+toServeremployeekey+'&OrganizationID='+OrganizationID);
+  }
   getMeetingTrainingViewforemployee(page, count, curr_date, empKey, orgID) {
     return this
       .http
