a88874135af594fe051bf4969819e978c2028f82
rodneyThomas <rodney.troodon@outlook.com>
2018-12-25 23:48:08 +0530

    styling

--- src/app/control-and-view/manager/reports/dashboard-report/dashboard-report.component.ts
+++ src/app/control-and-view/manager/reports/dashboard-report/dashboard-report.component.ts
@@ -54,7 +54,7 @@ export class DashboardReportComponent implements OnInit {
     barTitleIfEmpty: 'Click to select a date',
     placeholder: 'Click to select a date', // HTML input placeholder attribute (default: '')
     addClass: '', // Optional, value to pass on to [ngClass] on the input field
-    addStyle: { 'font-size': '18px', 'width': '101%',  'border': '1px solid #ced4da', 'border-radius': '0.25rem' }, // Optional, value to pass to [ngStyle] on the input field
+    addStyle: { 'font-size': '18px', 'width': '125%',  'border': '1px solid #ced4da', 'border-radius': '0.25rem' }, // Optional, value to pass to [ngStyle] on the input field
     fieldId: 'my-date-picker', // ID to assign to the input field. Defaults to datepicker-<counter>
     useEmptyBarTitle: false, // Defaults to true. If set to false then barTitleIfEmpty will be disregarded and a date will always be shown 
   };
