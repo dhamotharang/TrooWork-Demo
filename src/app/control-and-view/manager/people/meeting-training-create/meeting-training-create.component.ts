a11b8f18a0b7eec159ca590af7596273d1b655c4
rodneyThomas <rodney.troodon@outlook.com>
2019-01-03 11:38:22 +0530

    styling

--- src/app/control-and-view/manager/people/meeting-training-create/meeting-training-create.component.ts
+++ src/app/control-and-view/manager/people/meeting-training-create/meeting-training-create.component.ts
@@ -93,7 +93,7 @@ export class MeetingTrainingCreateComponent implements OnInit {
     barTitleIfEmpty: 'Click to select a date',
     placeholder: 'Click to select a date', // HTML input placeholder attribute (default: '')
     addClass: '', // Optional, value to pass on to [ngClass] on the input field
-    addStyle: {'font-size':'18px','width':'88%', 'border': '1px solid #ced4da','border-radius': '0.25rem'}, // Optional, value to pass to [ngStyle] on the input field
+    addStyle: {'font-size':'18px','width':'36.8%', 'border': '1px solid #ced4da','border-radius': '0.25rem'}, // Optional, value to pass to [ngStyle] on the input field
     fieldId: 'my-date-picker', // ID to assign to the input field. Defaults to datepicker-<counter>
     useEmptyBarTitle: false, // Defaults to true. If set to false then barTitleIfEmpty will be disregarded and a date will always be shown 
   };
